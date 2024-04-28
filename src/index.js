94a6e8dbb6f3ea87c3c0639591c5683ec881328d
Md Ahadullah <ahad53344@gmail.com>
2024-04-28 13:42:11 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ dotenv.config({
 })
 
 
-connectDB()
+connectDB().then(()=>{
+    app.listen(process.env.PORT||3000);
+    console.log("server is running at port:")
+})
+.catch((err)=>{
+    console.log("mongo db connection failed !!", err)
+})
 
 
 
